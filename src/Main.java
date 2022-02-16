e693438163d84e899fe42e7e6502a745c9ccb97a
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-16 05:38:26 +0100

    - Refactor day method to add boolean for Challenge A or B - Implement challenge B

--- src/Main.java
+++ src/Main.java
@@ -4,17 +4,20 @@ import java.io.FileNotFoundException;
 import java.util.Arrays;
 import java.util.Scanner;
 
+
 public class Main {
   public static void main(String args[]) throws FileNotFoundException {
-    day1();
+    day1(false);
   }
 
-  public static void day1() {
-//    int[] readings = getInputAsInt("./src/data/day1-sample.txt");
-    int[] readings = getInputAsInt("./src/data/day1-input.txt");
+  public static void day1(boolean challengeA) {
+    int[] readings = getInputAsInt("./src/data/day1-sample.txt");
+//    int[] readings = getInputAsInt("./src/data/day1-input.txt");
     int count = 0;
+    int pointerAndLookback = challengeA ? 1 : 3;
+
     for (int i = 1; i < readings.length; i++) {
-      count += readings[i] > readings[i -1] ? 1 : 0 ;
+      count += readings[i] > readings[i - 1] ? 1 : 0;
     }
     System.out.println(Arrays.toString(readings));
     System.out.println(count);
