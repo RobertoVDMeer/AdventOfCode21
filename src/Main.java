93ab4dec988bb0e5e43ba008cb81ed0caddd70d0
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-18 08:20:45 +0100

    First implementation day4 A, hope it works and pray to who- or whatever will listen that part 2 is just a change in grid size

--- src/Main.java
+++ src/Main.java
@@ -167,23 +167,57 @@ public class Main {
     // 8 legged bastard :p (yes I know that's an octopus, but they don't play
     // bingo either now do they, wise-ass?)
     final int CARDS = 3;
-    final int GRID = 5;
+    // make the cards 1 bigger to keep an index on pos 0 row / col
+    final int GRID_SIZE = 6;
+    int winningCard = 0;
+    int winningNum = 0;
+    int sum = 0;
+
+    int[][][] cards = new int[CARDS][GRID_SIZE][GRID_SIZE];
     int[] draws = Arrays.stream(input[0].split(","))
         .mapToInt(Integer::parseInt)
         .toArray();
-    int[][][] cards = new int[CARDS][GRID][GRID];
+
+    // fill the cards
     int fillPointer = 1;
     for (int card = 0; card < CARDS; card++) {
-      for (int row = 0; row < GRID; row++) {
-        for (int col = 0; col < GRID; col++) {
+      for (int row = 1; row < GRID_SIZE; row++) {
+        for (int col = 1; col < GRID_SIZE; col++) {
           cards[card][row][col] = Integer.parseInt(input[fillPointer]);
           fillPointer++;
         }
       }
     }
 
-    System.out.println(Arrays.toString(draws));
-    System.out.println(Arrays.toString(cards[2][4]));
+    out_game:
+    // play
+    for (int i = 0; i < draws.length; i++) {
+      for (int card = 0; card < CARDS; card++) {
+        for (int row = 1; row < GRID_SIZE; row++) {
+          for (int col = 1; col < GRID_SIZE; col++) {
+            if (draws[i] == cards[card][row][col]) {
+              cards[card][row][col] = -1;
+              cards[card][row][0]++;
+              cards[card][0][col]++;
+              System.out.println("found " + draws[i]);
+              if (cards[card][row][0] == 5 || cards[card][0][col] == 5) {
+                winningCard = card;
+                winningNum = draws[i];
+                System.out.println("Card " + (card + 1) + " won with number " + draws[i]);
+                break out_game;
+              }
+            }
+          }
+        }
+      }
+    }
+
+    // get the sum of all elements, skip the first row as it only indexed the hits
+    // in cols, but the first cols does the same for rows
+    for (int row = 1; row < GRID_SIZE; row++) {
+       sum += Arrays.stream(cards[winningCard][row]).sum();
+    }
+    System.out.println(winningNum * sum);
   }
 
   public static void dayX(boolean challengeA) {
