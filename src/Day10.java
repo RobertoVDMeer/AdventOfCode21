bbe63166f230a9581ca4f3fad38c53f412af17b2
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-24 08:49:45 +0100

    First implementation of day 10 A

--- src/Day10.java
+++ src/Day10.java
@@ -1,9 +1,40 @@
-import java.util.Arrays;
+import java.util.*;
 
 public class Day10 extends Base {
   public static void solve(CHALLENGE challenge) {
-    String[] input = getInputAsString("./src/data/day10-sample.txt");
-//    String[] input = getInputAsString("./src/data/day10-input.txt");
+//    String[] input = getInputAsString("./src/data/day10-sample.txt");
+    String[] input = getInputAsString("./src/data/day10-input.txt");
     System.out.println(Arrays.toString(input));
+
+    HashMap<Character,Character> legalClosers = new HashMap<>();
+    legalClosers.put(')', '('); legalClosers.put(']', '[');
+    legalClosers.put('}', '{'); legalClosers.put('>', '<');
+
+    HashMap<Character, Integer> scoreValues = new HashMap<>();
+    scoreValues.put(')', 3); scoreValues.put(']', 57);
+    scoreValues.put('}', 1197); scoreValues.put('>', 25137);
+
+    Stack<Character> openerStack = new Stack<>();
+    long score = 0;
+
+    for (String row : input) {
+      Iterator<Integer> c = row.chars().iterator();
+      corrupt_line:
+      while(c.hasNext()) {
+        char car = (char)c.next().intValue();
+        if(!legalClosers.containsKey(car)) {
+          openerStack.add(car);
+        } else {
+          if(openerStack.peek() == legalClosers.get(car)) {
+            openerStack.pop();
+          } else {
+            score += scoreValues.get(car);
+            break corrupt_line;
+          }
+        }
+      }
+    }
+
+    System.out.println(score);
   }
 }
