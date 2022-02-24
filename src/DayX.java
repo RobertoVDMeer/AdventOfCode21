3709fd1781100338eb339c3dbe8ca34b4645f8a2
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-24 07:21:35 +0100

    Refactor to have every day sit in its own file

---
+++ src/DayX.java
@@ -0,0 +1,9 @@
+public class DayX extends Base {
+  public static void solve(CHALLENGE challenge) {
+//    int[] input = getInputAsInt("./src/data/dayx-sample.txt");
+//    int[] input = getInputAsInt("./src/data/dayx-input.txt");
+//    String[] input = getInputAsString("./src/data/dayx-sample.txt");
+//    String[] input = getInputAsString("./src/data/dayx-input.txt");
+//    System.out.println(Arrays.toString(input));
+  }
+}
