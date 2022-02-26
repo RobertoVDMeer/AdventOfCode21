66a4cf7af5c925cf83c319ddc5e23dbc73b63566
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-26 08:31:29 +0100

    Start day 10 Challenge B

--- src/Day10.java
+++ src/Day10.java
@@ -11,8 +11,17 @@ public class Day10 extends Base {
     legalClosers.put('}', '{'); legalClosers.put('>', '<');
 
     HashMap<Character, Integer> scoreValues = new HashMap<>();
-    scoreValues.put(')', 3); scoreValues.put(']', 57);
-    scoreValues.put('}', 1197); scoreValues.put('>', 25137);
+    if (challenge == CHALLENGE.A) {
+      scoreValues.put(')', 3);
+      scoreValues.put(']', 57);
+      scoreValues.put('}', 1197);
+      scoreValues.put('>', 25137);
+    } else {
+      scoreValues.put(')', 1);
+      scoreValues.put(']', 2);
+      scoreValues.put('}', 3);
+      scoreValues.put('>', 4);
+    }
 
     Stack<Character> openerStack = new Stack<>();
     long score = 0;
@@ -22,6 +31,7 @@ public class Day10 extends Base {
       corrupt_line:
       while(c.hasNext()) {
         char car = (char)c.next().intValue();
+
         if(!legalClosers.containsKey(car)) {
           openerStack.add(car);
         } else {
@@ -29,10 +39,13 @@ public class Day10 extends Base {
             openerStack.pop();
           } else {
             score += scoreValues.get(car);
+            openerStack.clear();
             break corrupt_line;
           }
         }
       }
+      // This is now a legal, but unfinished line, e.g. Challenge B
+      System.out.println(openerStack.size());
     }
 
     System.out.println(score);
