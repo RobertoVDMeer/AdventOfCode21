0beefe37891aa037a4dba3a336141017c3367b7a
Roberto van der Meer <robertovdmeer@gmail.com>
2022-02-27 21:30:06 +0100

    create matrix from input

--- src/Day11.java
+++ src/Day11.java
@@ -1,10 +1,23 @@
 import java.util.*;
 
 public class Day11 extends Base {
+  static String[] input = getInputAsString("./src/data/day11-sample.txt");
+  //  static String[] input = getInputAsString("./src/data/day11-input.txt");
+  static int[][] matrix;
 
   public static void solve(CHALLENGE challenge) {
-    String[] input = getInputAsString("./src/data/day11-sample.txt");
-//    String[] input = getInputAsString("./src/data/day11-input.txt");
     System.out.println(Arrays.toString(input));
+    setMatrix();
+  }
+
+  private static void setMatrix() {
+    // array length for rows, StringLength for cols
+    matrix = new int[input.length][input[0].length()];
+
+    for (int r = 0; r < input.length; r++) {
+      for (int c = 0; c < input[r].length(); c++) {
+        matrix[r][c] = Integer.parseInt(input[r], c, c + 1, 10);
+      }
+    }
   }
 }
