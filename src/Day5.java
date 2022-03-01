0611140f3cae365e8b0e8e1d20bacd10aea3e73e
Roberto van der Meer <robertovdmeer@gmail.com>
2022-03-01 13:54:19 +0100

    Working on day 5 A - implemented day 5 A

--- src/Day5.java
+++ src/Day5.java
@@ -1,59 +1,63 @@
 import java.awt.*;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashSet;
+import java.util.*;
 
 public class Day5 extends Base {
   static String[] input = getInputAsString("./src/data/day5-sample.txt");
-//  static String[] input = getInputAsString("./src/data/day5-input.txt");
+  //  static String[] input = getInputAsString("./src/data/day5-input.txt");
+
+  static ArrayList<Point[]> inputAsPoints = new ArrayList<>();
+  static Map<String, Integer> pointMap = new HashMap<>();
+  static int totalOverlaps = 0;
 
   public static void solve(CHALLENGE challenge) {
-    System.out.println(Arrays.toString(input));
-
-    ArrayList<Point[]> inputAsPoints = parseInputAsPoints();
-
-    HashSet<Point> pointSet = new HashSet<>();
-    int totalOverlaps = 0;
-
-    totalOverlaps += fillPointSet(pointSet, input);
-
+    parseInputAsPoints();
+    drawStraightLines();
+    System.out.println(totalOverlaps);
   }
 
-  private static ArrayList<Point[]> parseInputAsPoints() {
-    ArrayList<Point[]> points = new ArrayList<>();
+  private static void drawStraightLines() {
+    Iterator<Point[]> pIter = inputAsPoints.iterator();
+    while (pIter.hasNext()) {
+      Point[] p = pIter.next();
+
+      if (p[0].x != p[1].x && p[0].y != p[1].y) continue;
+
+      boolean lastOne;
+      do {
+        lastOne = p[0].equals(p[1]);
+        Integer count = pointMap.get(p[0].toString());
+
+        if (count != null) count++;
+        else count = 1;
+        if (count == 2) totalOverlaps++;
+
+        pointMap.put(p[0].toString(), count);
+
+        if (p[0].x < p[1].x) p[0].move(p[0].x + 1, p[0].y);
+        if (p[0].x > p[1].x) p[0].move(p[0].x - 1, p[0].y);
+        if (p[0].y < p[1].y) p[0].move(p[0].x, p[0].y + 1);
+        if (p[0].y > p[1].y) p[0].move(p[0].x, p[0].y - 1);
+
+      } while (!lastOne);
+    }
+  }
+
+  private static void parseInputAsPoints() {
     for (int i = 0; i < input.length; i++) {
-      if(input[i].equals("->")) {
+      if (input[i].equals("->")) {
         ArrayList<Point> pointPair = new ArrayList<>();
         // add the prev and next X and Y as points
         for (int p = i - 1; p < i + 2; p += 2) {
           String[] xy = input[p].split(",");
           pointPair.add(new Point(
               Integer.parseInt(xy[0])
-              , Integer.parseInt(xy[0])
+              , Integer.parseInt(xy[1])
           ));
-          points.add(pointPair.toArray(Point[]::new));
         }
+        inputAsPoints.add(pointPair.toArray(Point[]::new));
         i++;
       }
     }
-    /*
-    final int MOD_X_1 = 1;
-    final int MOD_X_2 = 4;
-    for (int i = 0; i < input.length; i++) {
-      int mod = (i + 1) % 5;
-      if (mod == MOD_X_1 || mod == MOD_X_2) {
-        int x = Integer.parseInt(input[i]);
-        int y = Integer.parseInt(input[i + 1]);
-        points.add(new Point(x, y));
-      }
-    }
-
-     */
-    return points;
   }
 
-  private static int fillPointSet(HashSet<Point> pointSet, String[] input) {
-    int overlaps = 0;
-    return overlaps;
-  }
 }
