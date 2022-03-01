5af54ff64a429af0078003a11d78f77674b80c33
Roberto van der Meer <robertovdmeer@gmail.com>
2022-03-01 05:58:04 +0100

    working on day 11 - finished day 11 A

--- src/Day11.java
+++ src/Day11.java
@@ -1,90 +1,31 @@
+import java.text.MessageFormat;
 import java.util.*;
 
 public class Day11 extends Base {
-  static String[] input = getInputAsString("./src/data/day11-sample.txt");
-  //  static String[] input = getInputAsString("./src/data/day11-input.txt");
+      static String[] input = getInputAsString("./src/data/day11-sample.txt");
+//  static String[] input = getInputAsString("./src/data/day11-input.txt");
 
-  enum checkDirection {NW, N, NE, W, C, E, SW, S, SE}
-  static HashMap<checkDirection, checkDirection> nextDirection = new HashMap<>();
+  enum positionGrid {NW, N, NE, W, C, E, SW, S, SE}
+
+  static HashMap<positionGrid, positionGrid> nextPosition = new HashMap<>();
   static int[][] matrix;
+  static int flashes;
 
   public static void solve(CHALLENGE challenge) {
-    int flashes = 0;
+    flashes = 0;
     System.out.println(Arrays.toString(input));
+    int steps = 100;
     setMatrix();
-    setNextDirection();
-    printMatrix();
-    raiseEnergy();
-    printMatrix();
-    flashes += checkFlashes();
-    printMatrix();
-    System.out.println(flashes);
-  }
+    setNextPositionMap();
 
-  private static void setNextDirection() {
-    nextDirection.put(checkDirection.C, checkDirection.NW);
-    nextDirection.put(checkDirection.NW, checkDirection.N);
-    nextDirection.put(checkDirection.N, checkDirection.NE);
-    nextDirection.put(checkDirection.NE, checkDirection.W);
-    nextDirection.put(checkDirection.W, checkDirection.E);
-    nextDirection.put(checkDirection.E, checkDirection.SW);
-    nextDirection.put(checkDirection.SW, checkDirection.S);
-    nextDirection.put(checkDirection.S, checkDirection.SE);
-  }
-
-
-  private static int checkFlashes() {
-    int flashes = 0;
-    for (int r = 0; r < matrix.length; r++) {
-      for (int c = 0; c < matrix[r].length; c++) {
-        if (matrix[r][c] > 8) {
-          flashes += flashFlood(r, c, checkDirection.C);
-        }
-      }
+    for (int i = 0; i < steps; i++) {
+      raiseEnergy();
+      checkFlashes();
     }
-    return flashes;
-  }
+    printMatrix();
 
-  private static int flashFlood(int r, int c, checkDirection direction) {
-    int flashes = 0;
-
-    if (matrix[r][c] > 8) {
-      flashes++;
-      matrix[r][c] = -1;
-    } else if (matrix[r][c] != -1) {
-      matrix[r][c]++;
-    }
-    System.out.println(direction);
-    switch (direction) {
-      case C:
-        if (r - 1 > 0 && c - 1 > 0) {
-          flashFlood(r - 1, c - 1, nextDirection.get(direction));
-        }
-        break;
-      case NW:
-      case SW:
-      case N:
-      case S:
-        if (c + 1 < matrix[r].length) {
-          flashFlood(r, c + 1, nextDirection.get(direction));
-        }
-        break;
-      case W:
-        if (c + 2 < matrix[r].length) {
-          flashFlood(r, c + 2, nextDirection.get(direction));
-        }
-        break;
-      case E:
-      case NE:
-        if (r + 1 < matrix.length && c - 2 > 0) {
-          flashFlood(r + 1, c - 2, nextDirection.get(direction));
-        }
-        break;
-      case SE:
-        // this is the last one, showing the case for sanity
-    }
-
-    return flashes;
+    System.out.println(MessageFormat.format("Flashes after {0} steps: {1}"
+        , steps, flashes));
   }
 
   private static void raiseEnergy() {
@@ -92,6 +33,56 @@ public class Day11 extends Base {
       for (int c = 0; c < r.length; c++) {
         r[c]++;
       }
+    }
+  }
+
+  private static void checkFlashes() {
+    for (int r = 0; r < matrix.length; r++) {
+      for (int c = 0; c < matrix[r].length; c++) {
+        if (matrix[r][c] > 9) {
+          flashes++;
+          matrix[r][c] =0;
+          flashSplash(r, c, positionGrid.C);
+        }
+      }
+    }
+  }
+
+  private static void flashSplash(int r, int c, positionGrid pos) {
+    if (pos != positionGrid.C
+        && r >= 0 && r < matrix.length
+        && c >= 0 && c < matrix[r].length) {
+
+      if (matrix[r][c] != 0) {
+        matrix[r][c]++;
+      }
+
+      if (matrix[r][c] > 9) {
+        flashes++;
+        matrix[r][c] = 0;
+        flashSplash(r, c, positionGrid.C);
+      }
+    }
+
+    switch (pos) {
+      case C:
+        flashSplash(r - 1, c - 1, nextPosition.get(pos));
+        break;
+      case NW:
+      case SW:
+      case N:
+      case S:
+        flashSplash(r, c + 1, nextPosition.get(pos));
+        break;
+      case W:
+        flashSplash(r, c + 2, nextPosition.get(pos));
+        break;
+      case E:
+      case NE:
+        flashSplash(r + 1, c - 2, nextPosition.get(pos));
+        break;
+      case SE:
+        // this is the last one, showing the case for sanity
     }
   }
 
@@ -104,6 +95,18 @@ public class Day11 extends Base {
         matrix[r][c] = Integer.parseInt(input[r], c, c + 1, 10);
       }
     }
+  }
+
+  private static void setNextPositionMap() {
+    nextPosition.put(positionGrid.C, positionGrid.NW);
+    nextPosition.put(positionGrid.NW, positionGrid.N);
+    nextPosition.put(positionGrid.N, positionGrid.NE);
+    nextPosition.put(positionGrid.NE, positionGrid.W);
+    nextPosition.put(positionGrid.W, positionGrid.E);
+    nextPosition.put(positionGrid.E, positionGrid.SW);
+    nextPosition.put(positionGrid.SW, positionGrid.S);
+    nextPosition.put(positionGrid.S, positionGrid.SE);
+    nextPosition.put(positionGrid.SE, positionGrid.C);
   }
 
   private static void printMatrix() {
