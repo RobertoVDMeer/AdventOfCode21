3acfa5ed0956c0fdb1e8bc9c2b99f9fff112b64c
Roberto van der Meer <robertovdmeer@gmail.com>
2022-03-03 10:48:40 +0100

    Day 7 - Working on challenge B

--- src/Day7.java
+++ src/Day7.java
@@ -16,7 +16,11 @@ public class Day7 extends Base {
     while(true) {
      int currTotal = 0;
      for(int i = 0; i < input.length; i++){
-       currTotal += Math.abs(input[pointer] - input[i]);
+       if(challenge == CHALLENGE.A) {
+         currTotal += Math.abs(input[pointer] - input[i]);
+       } else {
+         currTotal += calculateExponentCost(Math.abs(input[pointer]), Math.abs(input[i]));
+       }
      }
       System.out.println(currTotal);
      if (lowestTotal == 0) {
@@ -38,5 +42,16 @@ public class Day7 extends Base {
     }
     System.out.println(MessageFormat.format("pos {0} cost least for {1} fuel"
     , lowestPos, lowestTotal));
+  }
+
+  private static int calculateExponentCost(int to, int from) {
+    int result = 0;
+    int raise = 1;
+    if (to > from) {
+      while(to != from++) result += raise++;
+    } else if (to < from) {
+      while(to != from--) result += raise++;
+    }
+    return result;
   }
 }
