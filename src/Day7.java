f9c878c7292b0976ac2082246df1f15fb2120f83
Roberto van der Meer <robertovdmeer@gmail.com>
2022-03-03 10:21:56 +0100

    Day 7 - implemented challenge A

--- src/Day7.java
+++ src/Day7.java
@@ -1,9 +1,42 @@
+import java.text.MessageFormat;
 import java.util.Arrays;
 
 public class Day7 extends Base {
-    static int[] input = getInputAsCSVInt("./src/data/day7-sample.txt");
-//    static int[] input = getInputAsCSVInt("./src/data/day7-input.txt");
+//  static int[] input = getInputAsCSVInt("./src/data/day7-sample.txt");
+      static int[] input = getInputAsCSVInt("./src/data/day7-input.txt");
+
   public static void solve(CHALLENGE challenge) {
+    Arrays.sort(input);
     System.out.println(Arrays.toString(input));
+    int lowestTotal = 0;
+    int lowestPos = 0;
+    boolean pointerWentLeft = true;
+    int pointer = input.length / 2;
+
+    while(true) {
+     int currTotal = 0;
+     for(int i = 0; i < input.length; i++){
+       currTotal += Math.abs(input[pointer] - input[i]);
+     }
+      System.out.println(currTotal);
+     if (lowestTotal == 0) {
+       lowestTotal = currTotal;
+       lowestPos = input[pointer];
+       while(input[pointer] == input[--pointer]);
+     } else if(currTotal < lowestTotal) {
+       lowestTotal = currTotal;
+       if (pointerWentLeft) {
+         while(input[pointer] == input[--pointer]);
+       } else {
+         while(input[pointer] == input[++pointer]);
+       }
+     } else {
+       while(input[pointer] == input[++pointer]);
+       if(!pointerWentLeft) break;
+       pointerWentLeft = false;
+     }
+    }
+    System.out.println(MessageFormat.format("pos {0} cost least for {1} fuel"
+    , lowestPos, lowestTotal));
   }
 }
